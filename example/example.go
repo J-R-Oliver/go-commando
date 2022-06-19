d4004b4ab5ce35efc98c8102b6ca5ad9434712f4
James Oliver <joliver.robert@me.com>
2022-06-19 08:54:16 +0100

    refactor: fix linting issues

--- example/example.go
+++ example/example.go
@@ -9,11 +9,13 @@ import (
 func main() {
 	argOptPrinter := func(arguments []string, options map[string]string) {
 		fmt.Println("Arguments:")
+
 		for i, a := range arguments {
 			fmt.Printf("\tindex: %d, argument: %s\n", i, a)
 		}
 
 		fmt.Println("Options:")
+
 		for k, v := range options {
 			fmt.Printf("\tkey: %s, option: %s\n", k, v)
 		}
